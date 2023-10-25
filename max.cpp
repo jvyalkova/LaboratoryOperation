66a0c2490d786f8da09d3484eead9f026cf1c510
jvyalkova <146427213+jvyalkova@users.noreply.github.com>
2023-10-25 17:13:37 +0300

    Update max.cpp

--- max.cpp
+++ max.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
-#include <vector>
-using namespace  std;
+using namespace std;
 
-int Max(const std::vector<int>& arr, int start, int end) {
-    if (start == end) {
-        return start;
+int findMax(int arr[], int start, int end, int& maxIndex)
+{
+    if (start == end)
+    {
+        maxIndex = start;
+        return arr[start];
     }
 
     int mid = (start + end) / 2;
-    int leftMax = Max(arr, start, mid);
-    int rightMax = Max(arr, mid + 1, end);
-
-    return (arr[leftMax] > arr[rightMax]) ? leftMax : rightMax;
+    int leftMax = findMax(arr, start, mid, maxIndex);
+    int rightMax = findMax(arr, mid + 1, end, maxIndex);
+    
+    if (leftMax >= rightMax) 
+    {
+        return leftMax;
+    }
+    else {
+        maxIndex = maxIndex + (mid - 1 - start);
+        return rightMax;
+    }
 }
 
-int main() {
-    
-    setlocale(LC_ALL, "Rus");
+int main()
+{
+    setlocale(LC_ALL, "RUS");
     int size;
-
     cout << "Введите размер массива: ";
     cin >> size;
 
-    vector<int> arr(size);
-
-    cout << "Введите элементы массива:"<< endl;
-    for (int i = 0; i < size; i++) {
-        cout << "Элемент " << i << ": ";
+    int* arr = new int[size];
+    cout << "Введите элементы массива через пробел: ";
+    for (int i = 0; i < size; i++)
         cin >> arr[i];
-    }
+    
+    int maxIndex;
+    int max = findMax(arr, 0, size - 1, maxIndex);
 
-    int maxIndex = Max(arr, 0, size - 1);
-    int maxValue = arr[maxIndex];
-
-    cout << "Максимальный элемент: " << maxValue << ", его индекс: " << maxIndex << endl;
-
+    cout << "Максимальный элемент массива: " << max << endl;
+    cout << "Индекс максимального элемента: " << maxIndex << endl;
+    delete[] arr;
     return 0;
 }
