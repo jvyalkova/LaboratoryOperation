eb5e4d27d258966db32381d8bdbcd7a788a0d2a3
jvyalkova <146427213+jvyalkova@users.noreply.github.com>
2023-10-29 17:50:23 +0300

    Update max.cpp

--- max.cpp
+++ max.cpp
@@ -1,24 +1,23 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
-int findMax(int arr[], int start, int end, int& maxIndex)
+int findMax(int arr[], int start, int end )
 {
     if (start == end)
     {
-        maxIndex = start;
         return arr[start];
     }
 
     int mid = (start + end) / 2;
-    int leftMax = findMax(arr, start, mid, maxIndex);
-    int rightMax = findMax(arr, mid + 1, end, maxIndex);
-    
-    if (leftMax >= rightMax) 
+    int leftMax = findMax(arr, start, mid);
+    int rightMax = findMax(arr, mid + 1, end);
+
+    if (leftMax >= rightMax)
     {
         return leftMax;
     }
-    else {
-        maxIndex = maxIndex + (mid - 1 - start);
+    else
+    {
         return rightMax;
     }
 }
@@ -29,17 +28,23 @@ int main()
     int size;
     cout << "Введите размер массива: ";
     cin >> size;
-
     int* arr = new int[size];
-    cout << "Введите элементы массива через пробел: ";
+    cout << "Сгенерированный массив: ";
     for (int i = 0; i < size; i++)
-        cin >> arr[i];
-    
-    int maxIndex;
-    int max = findMax(arr, 0, size - 1, maxIndex);
-
+    {
+        arr[i] = rand() % 100;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    int maxIndex = 0;
+    int max = findMax(arr, 0, size - 1 );
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == max)
+        {
+            maxIndex = i;
+        }
+    }
     cout << "Максимальный элемент массива: " << max << endl;
     cout << "Индекс максимального элемента: " << maxIndex << endl;
-    delete[] arr;
-    return 0;
 }
